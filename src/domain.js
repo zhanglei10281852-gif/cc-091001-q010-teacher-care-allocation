7df6796cc32b920be46071c8761e18374073ec46
Pairwise baseline migration <pairwise-baseline@localhost>
2000-01-01 00:00:00 +0000

    A/B baseline migrated from 25ce8140abbd88cda4545f1eb5cfe91343b4317c

---
+++ src/domain.js
@@ -0,0 +1,4 @@
+export const resourceKinds = ['counselling', 'temporary-load-relief', 'peer-support'];
+export const applicationStates = ['submitted', 'eligible', 'waiting', 'allocated', 'closed', 'withdrawn'];
+export const reviewStates = ['none', 'appealed', 'reviewing', 'resolved'];
+export const urgencyLevels = ['standard', 'priority', 'immediate'];
